93daedf624dfba2464a92eb42a4ba8643c8d3130
roskey <lsjsg@outlook.com>
2021-04-03 23:21:17 +0800

    Added Cup

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,8 @@
-import React, { useState } from "react";
+import React, {useState} from "react";
 import Welcome from './pages/welcome';
 import 'antd/dist/antd.css';
 import './App.css';
+import Cup from "./pages/cup";
 
 function SamplePage(props) {
     return (
@@ -18,11 +19,12 @@ function App() {
         setStage(stage + 1);
     }
     const stages = {
-        0: <Welcome afterSubmit={nextStage} />,
-        1: <SamplePage title="Page 2" nextStage={nextStage} />,
-        2: <SamplePage title="Page 3" nextStage={nextStage} />,
-        3: <SamplePage title="Page 4" nextStage={nextStage} />,
-        4: <SamplePage title="Page 5" nextStage={nextStage} />,
+        0: <Cup initialPos={0} nextStage={nextStage}/>,
+        // 0: <Welcome afterSubmit={nextStage} />,
+        1: <SamplePage title="Page 2" nextStage={nextStage}/>,
+        2: <SamplePage title="Page 3" nextStage={nextStage}/>,
+        3: <SamplePage title="Page 4" nextStage={nextStage}/>,
+        4: <SamplePage title="Page 5" nextStage={nextStage}/>,
     };
     return stages[stage];
 }
--- client/src/pages/cup.js
+++ client/src/pages/cup.js
@@ -1,18 +1,75 @@
 import React from "react";
 import cup from "../image/cup.png";
-import {DndProvider} from "react-dnd";
-import { HTML5Backend } from 'react-dnd-html5-backend'
 
-function Cup() {
-    console.log(cup);
-    return (
-        <div className="container">
-            <h1>Shake the cup</h1>
-            <DndProvider backend={HTML5Backend}>
-                <img src={cup} alt="CUP"/>
-            </DndProvider>
-        </div>
-    );
+class Cup extends React.Component {
+    // https://stackoverflow.com/a/63102915
+    constructor(props) {
+        super(props);
+        this.myRef = React.createRef();
+        this.state = {
+            counter: this.props.counter,
+            pos: this.props.initialPos,
+            dragging: false,
+            rel: null
+        };
+        this.proceed = props.nextStage;
+    }
+
+    componentDidUpdate(props, state) {
+        if (this.state.dragging && !state.dragging) {
+            document.addEventListener('mousemove', this.onMouseMove);
+            document.addEventListener('mouseup', this.onMouseUp);
+        } else if (!this.state.dragging && state.dragging) {
+            document.removeEventListener('mousemove', this.onMouseMove);
+            document.removeEventListener('mouseup', this.onMouseUp);
+        }
+    }
+
+    onMouseDown = (e) => {
+        console.log("Mouse down");
+        if (e.button !== 0) return;
+        let pos = {left: this.myRef.current.offsetLeft, top: this.myRef.current.offsetTop}
+        this.setState({
+            dragging: true,
+            rel: {
+                x: e.pageX - pos.left,
+                y: e.pageY - pos.top
+            }
+        });
+        e.stopPropagation();
+        e.preventDefault();
+    }
+
+    onMouseUp = (e, props) => {
+        console.log("Mouse up")
+        this.setState({dragging: false});
+        e.stopPropagation();
+        e.preventDefault();
+        this.proceed();
+    }
+
+    onMouseMove = (e) => {
+        if (!this.state.dragging) return;
+
+        this.setState({
+            pos: {
+                x: e.pageX - this.state.rel.x,
+                y: e.pageY - this.state.rel.y
+            }
+        });
+        e.stopPropagation();
+        e.preventDefault();
+    }
+
+
+    render() {
+        return (
+            <span ref={this.myRef} onMouseDown={this.onMouseDown}
+                  style={{position: 'absolute', left: this.state.pos.x + 'px', top: this.state.pos.y + 'px'}}>
+                <img src={cup}/>
+            </span>
+        )
+    }
 }
 
 export default Cup;
