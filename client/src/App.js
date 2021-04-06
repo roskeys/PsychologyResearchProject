35b60cef3da94614b013835afb4dc733ad02febb
roskeys <lsjsg@outlook.com>
2021-04-06 11:45:37 +0800

    Changed the experiment file and make it customizable. # TODO change the css styles

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,10 @@
 import React, {useState} from "react";
 import Welcome from './pages/welcome';
+import Exp from "./pages/experiment";
 import 'antd/dist/antd.css';
 import './App.css';
 import Cup from "./pages/cup";
+import number6 from "./image/number6.png";
 import Introduction from "./pages/introduction";
 import Thank from "./pages/thanks";
 import {experiment1_intro, experiment2_intro, experiment3_intro} from "./introductions";
@@ -22,8 +24,10 @@ function App() {
     }
 
     const stages = {
-        0: <Welcome afterSubmit={nextStage}/>,
-        1: <Introduction num="1" message={experiment1_intro} nextStage={nextStage}/>,
+        0: <Exp path={number6} left={true} nextStage={nextStage}/>,
+        1: <Exp path={number6} left={false} nextStage={nextStage}/>,
+        3: <Welcome afterSubmit={nextStage}/>,
+        4: <Introduction num="1" message={experiment1_intro} nextStage={nextStage}/>,
         5: <Cup initialPos={0} nextStage={nextStage}/>,
         35: <Introduction num="2" message={experiment2_intro} nextStage={nextStage}/>,
         55: <Introduction num="3" message={experiment3_intro} nextStage={nextStage}/>,
--- client/src/pages/experiment.js
+++ client/src/pages/experiment.js
@@ -1,23 +1,58 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
 import 'antd/dist/antd.css';
-// import './index.css';
-import { Image } from 'antd';
-import { Button } from 'antd';
+import {Image} from 'antd';
+import {Button} from 'antd';
+import "../App.css";
 
 function Exp(props) {
-  return (
-    <div className = "Exp">
-        <Image
-        width={200}
-        src={props.path}
-        />
-        <Button>
-        Next
-        </Button>
-    </div>
-  );
+    let left = props.left ? "1" : "0";
+    let right = !props.left ? "1" : "0";
+    return (
+        <div className="outer-background">
+            <img
+                src={props.path}
+                style={{
+                    width: "40%",
+                    height: "40%",
+                    minWidth: "50px",
+                    minHeight: "50px",
+                    maxWidth: "250px",
+                    maxHeight: "250px",
+                    opacity: left,
+                    paddingLeft: "5%",
+                    float: "left"
+                }}
+                alt={props.path}
+            />
+            <br/>
+            <img
+                width={200}
+                height={200}
+                src={props.path}
+                style={{
+                    width: "40%",
+                    height: "40%",
+                    minWidth: "50px",
+                    minHeight: "50px",
+                    maxWidth: "250px",
+                    maxHeight: "250px",
+                    opacity: right,
+                    paddingRight: "5%",
+                    float: "right"
+                }}
+            />
+            <div style={{
+                display: "block",
+                clear: "both",
+                paddingTop: "10%"
+            }}/>
+            <Button
+                className="submit-button"
+                onClick={props.nextStage}>
+                Next
+            </Button>
+        </div>
+    );
 }
 
-// ReactDOM.render(<ImageDemo />, mountNode);
 export default Exp;
