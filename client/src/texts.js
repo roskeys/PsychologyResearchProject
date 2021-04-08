15eaed0ce1a7cee8a185fb3bacd84f5a0a21cb06
Mulie23 <47581954+Mulie23@users.noreply.github.com>
2021-04-08 21:30:03 +0800

    Update texts.js

--- client/src/texts.js
+++ client/src/texts.js
@@ -47,9 +47,9 @@ export const experiment2_intro_2 = <div style={style}>
 
 export const experiment3_intro = <div style={style}>
     <li>In this experiment, you will see 25 images of smiling faces in sequence.</li>
-    <li>After viewing each smiling face, press the “Next” button to proceed to the next round.</li>
+    <li>After viewing each smiling face, press the <b>“Next”</b> button to proceed to the next round.</li>
     <li><b>Please move on to the next image as fast as possible.</b></li>
     <li>After viewing the 25 smiling faces, please submit your estimation of the <b>probability for smiling faces
-        showing up on the left</b> using the scrollbar and press the “Submit” button.
+        showing up on the left</b> using the scrollbar and press the <b>“Submit”</b> button.
     </li>
 </div>;
